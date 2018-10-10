d5258450aa3041cb2a927dfcafafa39f442e533a
vaibhav <vbvrocks84@gmail.com>
2018-10-10 17:03:25 -0500

    basic client done

---
+++ client/src/components/CurrentDate.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import './CurrentDate.css'
+class CurrentDate extends Component{
+
+    constructor()
+    {
+        super();
+        let today = moment() ;
+        let currentMonth = today.format("MMM");
+        let currentDate = today.format("D");
+        console.log(currentDate,currentMonth);
+
+        this.state = {
+            currentMonth: currentMonth,
+            currentDate: currentDate
+        };
+    }
+    render(){
+        return(
+            <div>
+                <div className="todaysDate">
+                    <div className="box">
+                    <p className="month">{this.state.currentMonth}</p>
+                    <p className="dayOfMonth">{this.state.currentDate}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CurrentDate;
+
